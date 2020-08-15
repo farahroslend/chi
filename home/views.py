7fa71d1204d343e98cd7a8578a1feea3b66b9761
farahroslend <farahroslend@gmail.com>
2020-08-15 10:17:34 +0000

    added additional doc jinja into base.html

--- home/views.py
+++ home/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
-def index (request):
+
+
+def index(request):
     """A view to return the index page"""
-    return render (request,'home/index.html')
+    return render(request, 'home/index.html')
