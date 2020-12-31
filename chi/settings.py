1ee95c1eeaf6a53de3e7494bac3bc48f5eddf5a6
farahroslend <farahroslend@gmail.com>
2020-12-31 16:54:56 +0000

    added whitenoise.storage.CompressedManifestStaticFilesStorage

--- chi/settings.py
+++ chi/settings.py
@@ -186,7 +186,7 @@ STATICFILES_DIRS = (
 
 STATIC_ROOT = os.path.join(BASE_DIR, "live-static-files", "static-root")
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 
 
